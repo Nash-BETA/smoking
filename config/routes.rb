d6a435684bd9955e58a7be9a28a55f3480debd7d
Nash-BEAT <nash.0cal@gmail.com>
2019-08-10 16:39:00 +0900

    hozon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+  get '/about', to: 'static#about'
+  get 'static/help'
 end
