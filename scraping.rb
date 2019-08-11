79cf440519346d05a1cb2c99672577b04fb28e1d
Nash-BEAT <nash.0cal@gmail.com>
2019-08-11 15:25:53 +0900

    URLの複数クローリング

--- scraping.rb
+++ scraping.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
+
 uri = "https://tabelog.com/kanagawa/A1401/A140105/14000514/"
 doc = Nokogiri::HTML(open(uri),nil,"utf-8")
 
@@ -16,3 +17,4 @@ end
 doc.xpath('//*[@id="contents-rstdata"]/div[2]/table[2]/tbody/tr[4]/td/text()').each do |smoking|
     puts smoking.text
 end
+
