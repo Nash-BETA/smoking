12ed478b59fd671de861576ce633924755a982f9
Nash-BEAT <nash.0cal@gmail.com>
2019-08-11 17:33:31 +0900

    DB作成

---
+++ db/migrate/20190811082832_create_stores.rb
@@ -0,0 +1,12 @@
+class CreateStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stores do |t|
+      t.integer :id
+      t.string :name
+      t.string :name_hurigana
+      t.string :smoking
+
+      t.timestamps
+    end
+  end
+end
