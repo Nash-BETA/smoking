c6132adb2d18a984e564e51a62ad507b443fbad6
Nash-BEAT <nash.0cal@gmail.com>
2019-08-29 00:28:03 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   get '/about',          to: 'static#about'
   get '/static/help',    to: 'static#help'
   get '/pref/:prefcode', to: 'prefecture#show'
-  #/1で北海道ページに飛ぶ
   get '/detail/z_:id',   to: 'store#detail'
+  get ':controller(/:action(/:id(.:format)))'
 
-  #get ':controller(/:action(/:id(.:format)))'
-  #controllerアクション
-  #/fetch でお店のクローリング　/prefectureで都道府県のクローリング  
-  get '/prefecture', to: 'prefecture#index'
-  get '/fetch' , to: 'fetch#index'
 end
