4f5a8e9e248fb2dbb04e7d5baa5ef93f3fb65a64
Nash-BEAT <nash.0cal@gmail.com>
2019-08-28 16:17:39 +0900

    controllerの作成およびJSONの短縮化

--- pre_scraping.rb
+++ pre_scraping.rb
@@ -2,28 +2,23 @@ require 'net/http'
 require 'uri'
 require 'json' 
 
-def get_json(url)
-
-  uri = URI.parse(URI.escape(url))
-
-  https = Net::HTTP.new(uri.host, uri.port)
-  https.use_ssl = true
-
-  param = {}
-  param['X-API-KEY'] = 'ChHeCpasyo0ZD74UeX8R3cgHId6KFScQ2tFFWcQN'
-
-  req = Net::HTTP::Get.new(uri.request_uri, param)
-  res = https.request(req)
-
-  json = res.body
-  result = JSON.parse(json)
-
-end
 
 
-data = get_json("https://opendata.resas-portal.go.jp/api/v1-rc.1/prefectures")
+uri = URI.parse('ttps://opendata.resas-portal.go.jp/api/v1-rc.1/prefectures')
 
-data["result"].each do |val|
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+
+param = {}
+param['X-API-KEY'] = 'ChHeCpasyo0ZD74UeX8R3cgHId6KFScQ2tFFWcQN'
+
+req = Net::HTTP::Get.new(uri.request_uri, param)
+res = https.request(req)
+
+json = res.body
+result = JSON.parse(json)
+
+result["result"].each do |val|
     puts val["prefCode"]
     puts val["prefName"]
 end
---
+++ test/controllers/prefecture_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PrefectureControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
