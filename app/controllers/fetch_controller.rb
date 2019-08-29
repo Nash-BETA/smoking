07eecc48bd5c6c8b5258ca1b6e6dbb073e0e5fae
Nash-BEAT <nash.0cal@gmail.com>
2019-08-29 18:20:11 +0900

    scraiping編集およびモデル追加

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -12,16 +12,20 @@ class FetchController < ApplicationController
         name = doc.css("h2.display-name").text
         name_hurigana = doc.css("span.alias").text
         smoking = doc.xpath('//*[@id="contents-rstdata"]/div[2]/table[2]/tbody/tr[4]/td/text()').text
-        insert_feed(name, name_hurigana, smoking)
+        prefecture = doc.xpath('//*[@id="contents-rstdata"]/div[2]/table[1]/tbody/tr[5]/td/p/span[1]/a')
+        city = doc.xpath('//*[@id="contents-rstdata"]/div[2]/table[1]/tbody/tr[5]/td/p/span[2]/a[1]')
+        insert_feed(name, name_hurigana, smoking,prefecture,city)
     end
 end
     
     private
-    def insert_feed(name, name_hurigana, smoking)
+    def insert_feed(name, name_hurigana, smoking,prefecture,city)
         store = Store.new(
             :name             => name,
             :name_hurigana    => name_hurigana,
             :smoking          => smoking,
+            :prefecture       => prefecture,
+            :city             => city,
             )
         store.save
     end
