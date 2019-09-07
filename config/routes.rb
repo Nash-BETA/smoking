17e20e74c20bfbeb972a15e3bc6adb833d2c97dd
Nash-BEAT <nash.0cal@gmail.com>
2019-09-07 19:08:35 +0900

    一覧リンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get '/about',          to: 'static#about'
   get '/static/help',    to: 'static#help'
   get '/pref/:prefcode', to: 'prefecture#show'
-  get '/detail/z_:id',   to: 'store#detail'
+  get '/detail/z_:id',   to: 'store#detail', as: 'detail'
+  #asでPrefix の設定ができる
   get ':controller(/:action(/:id(.:format)))'
 
 end
