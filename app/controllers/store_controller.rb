42a9875ad6a2b5a303e058b2867807f0c870a806
Nash-BEAT <nash.0cal@gmail.com>
2019-09-23 01:42:25 +0900

    口コミ書いた人の名前表示

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,10 +4,11 @@ class StoreController < ApplicationController
     @reviews         = Review.new
     @users           = current_user.id
     
-    st = detail_path[10,10].to_i
-    review_store = Review.where(store_id: "#{st}")
+    st              = detail_path[10,10].to_i
+    review_store    = Review.where(store_id: "#{st}")
     @review_display = review_store
 
+    @user_review    = User.all
 
 
   end
