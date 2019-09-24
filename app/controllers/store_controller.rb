c5b40d22366fe85d66521fff33ce85e2096435de
Nash-BEAT <nash.0cal@gmail.com>
2019-09-24 18:13:05 +0900

    画像表示完了

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,14 +3,12 @@ class StoreController < ApplicationController
     @stores          = Store.find(params[:id])
     @reviews         = Review.new
     @users           = current_user.id
+
     
     st              = detail_path[10,10].to_i
     review_store    = Review.where(store_id: "#{st}")
     @review_display = review_store
-
     @user_review    = User.all
 
-
   end
-
 end
