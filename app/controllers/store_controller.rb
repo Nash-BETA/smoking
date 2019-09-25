b9e054444fed40ede0d566a6fd1145dd580ef9ac
Nash-BEAT <nash.0cal@gmail.com>
2019-09-26 02:28:30 +0900

    非会員でも詳細ページに遷移できるように修正

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,9 @@ class StoreController < ApplicationController
   def detail
     @stores          = Store.find(params[:id])
     @reviews         = Review.new
-    @users           = current_user.id
+    if logged_in?
+      @users           = current_user.id
+    end
 
     
     st              = detail_path[10,10].to_i
