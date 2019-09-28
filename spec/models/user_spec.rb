11142abd04b7bf1cf28da92079565e39e21dd06a
Nash-BEAT <nash.0cal@gmail.com>
2019-09-28 14:26:20 +0900

    modelを複数でテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it "名前入力のテスト" do
-        user = User.new(
-                name: "テスト君"
-        )
-        expect(user.name).to eq "テスト君"
+    describe "名前入力" do
+        it "テスト君で入力" do
+            user_name = User.new(name: "テスト君")
+            expect(user_name.name).to eq "テスト君"
+        end
+    end
+    describe "入力" do
+        it "入力" do
+            user_email = User.new(email: "test@gmail.com")
+            expect(user_email.email).to eq "test@gmail.com"
+        end
     end
 end
+
