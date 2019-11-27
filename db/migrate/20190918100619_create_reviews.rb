421b3edda802c2deba621a99df30661ce580f908
Nash-BEAT <nash.0cal@gmail.com>
2019-11-27 22:11:11 +0900

    マイグレされ無いので一旦保存

--- db/migrate/20190918100619_create_reviews.rb
+++ db/migrate/20190918100619_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.text :content
-      t.references :user,  foreign_key: 
-      t.references :store, foreign_key:
+      t.references :user,  foreign_key: true
+      t.references :store, foreign_key: true
 
       t.timestamps
     end
