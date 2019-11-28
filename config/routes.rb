69bb2bc9d69e06c64a6c0dbb25ccd8f79110e53b
Nash-BEAT <nash.0cal@gmail.com>
2019-11-28 22:27:09 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/about',          to: 'static#about'
   get '/static/help',    to: 'static#help'
-  get '/pref/:prefcode', to: 'prefecture#show' ,as: 'pref'
+  get '/pref/:id', to: 'prefecture#show' ,as: 'pref'
   get '/detail/z_:id',   to: 'store#detail', as: 'detail'
 
   #ユーザーまわり
